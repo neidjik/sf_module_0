280f8096463698d258f4e5f77d336556fd3d11b7
neidjik <nturyeva@gmail.com>
2020-02-24 21:41:32 +0300

    Version4

--- game.py
+++ game.py
@@ -1,19 +1,28 @@
-version2
+import numpy as np
+
+def score_game(game_core_v1):
+    '''«апускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core_v1(number))
+    score = int(np.mean(count_ls))
+    print(f"¬аш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
 
 def game_core_v3(number):
-    import numpy as np
-    number=0
     count=0 # zero count
     n1=1 #begin of range
     n2=101 # end of range
-    predict = np.random.randint(1,100) # made a number
+    predict=0
     while number != predict:
-        number=(n1+n2) // 2 # count middle of range
+        predict=(n1+n2) // 2 # count middle of range
         count+=1
-        if number>predict:
-            n2=number # change range
+        if predict>number:
+            n2=predict # change range
         else:
-            n1=number # change range
+            n1=predict # change range
     return(count)
- 
+
 score_game(game_core_v3)
